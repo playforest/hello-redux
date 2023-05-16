5153d55afae106d5fd35f0abe8d4650396152f69
Imran Dhahbi <playforest0x@gmail.com>
2023-05-16 23:36:38 +1000

    add users slice and postSlice properties

--- bulletin-board/src/features/posts/PostsList.js
+++ bulletin-board/src/features/posts/PostsList.js
@@ -1,21 +1,29 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
-
+import PostAuthor from "./postAuthor";
+import TimeAgo from "./TimeAgo";
 const PostsList = () => {
-    const posts=useSelector(selectAllPosts)
+    const posts = useSelector(selectAllPosts)
 
-    const renderedPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
+            <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
         </article>
     ))
-  return (
-    <section>
-        <h2>Posts</h2>
-        {renderedPosts}
-    </section>
-  )
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {renderedPosts}
+        </section>
+    )
 }
 
 export default PostsList
--- bulletin-board/src/features/posts/postsSlice.js
+++ bulletin-board/src/features/posts/postsSlice.js
@@ -1,20 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
 
 const initialState = [
-    { id: '1', title: 'learning redux toolkit', content: "i've heard good things" },
-    { id: '2', title: 'slices...', content: 'the more i say slice, the more i want pizza' }
+    {
+        id: '1',
+        title: 'learning redux toolkit',
+        content: "i've heard good things",
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+    },
+    {
+        id: '2',
+        title: 'slices....',
+        content: "the more i say slice, the more i want pizza",
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+    }
 ]
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded(state, action) {
-            state.push(action.payload)
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        date: new Date().toISOString(),
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        },
+                        userId
+                    }
+                }
+            }
         }
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded } = postsSlice.actions
 export default postsSlice.reducer
